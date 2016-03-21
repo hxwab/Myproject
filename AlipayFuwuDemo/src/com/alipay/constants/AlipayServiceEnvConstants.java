1b5927a3b9be757dab6a529a323395522030eb23
hxwab <hxwab2010@126.com>
2016-03-21 09:15:06 +0800

    add

---
+++ AlipayFuwuDemo/src/com/alipay/constants/AlipayServiceEnvConstants.java
@@ -0,0 +1,43 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2014 All Rights Reserved.
+ */
+package com.alipay.constants;
+
+/**
+ * 支付宝服务窗环境常量（demo中常量只是参考，需要修改成自己的常量值）
+ * 
+ * @author taixu.zqq
+ * @version $Id: AlipayServiceConstants.java, v 0.1 2014年7月24日 下午4:33:49 taixu.zqq Exp $
+ */
+public class AlipayServiceEnvConstants {
+
+    /**支付宝公钥-从支付宝服务窗获取*/
+    public static final String ALIPAY_PUBLIC_KEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDDI6d306Q8fIfCOaTXyiUeJHkrIvYISRcc73s3vF1ZT7XN8RNPwJxo8pWaJMmvyTn9N4HQ632qJBVHf8sxHi/fEsraprwCtzvzQETrNRwVxLO5jVmRGi60j8Ue1efIlzPXV9je9mkjzOmdssymZkh2QhUrCmZYI/FCEa3/cNMW0QIDAQAB";
+
+    /**签名编码-视支付宝服务窗要求*/
+    public static final String SIGN_CHARSET      = "GBK";
+
+    /**字符编码-传递给支付宝的数据编码*/
+    public static final String CHARSET           = "GBK";
+
+    /**签名类型-视支付宝服务窗要求*/
+    public static final String SIGN_TYPE         = "RSA";
+
+    /** 服务窗appId  */
+    //TODO !!!! 注：该appId必须设为开发者自己的服务窗id  这里只是个测试id
+    public static final String APP_ID            = "2013102200001786";
+
+    //开发者请使用openssl生成的密钥替换此处  请看文档：https://fuwu.alipay.com/platform/doc.htm#2-1接入指南
+    //TODO !!!! 注：该私钥为测试账号私钥  开发者必须设置自己的私钥 , 否则会存在安全隐患 
+    public static final String PRIVATE_KEY       = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAKK0PXoLKnBkgtOl0kvyc9X2tUUdh/lRZr9RE1frjr2ZtAulZ+Moz9VJZFew1UZIzeK0478obY/DjHmD3GMfqJoTguVqJ2MEg+mJ8hJKWelvKLgfFBNliAw+/9O6Jah9Q3mRzCD8pABDEHY7BM54W7aLcuGpIIOa/qShO8dbXn+FAgMBAAECgYA8+nQ380taiDEIBZPFZv7G6AmT97doV3u8pDQttVjv8lUqMDm5RyhtdW4n91xXVR3ko4rfr9UwFkflmufUNp9HU9bHIVQS+HWLsPv9GypdTSNNp+nDn4JExUtAakJxZmGhCu/WjHIUzCoBCn6viernVC2L37NL1N4zrR73lSCk2QJBAPb/UOmtSx+PnA/mimqnFMMP3SX6cQmnynz9+63JlLjXD8rowRD2Z03U41Qfy+RED3yANZXCrE1V6vghYVmASYsCQQCoomZpeNxAKuUJZp+VaWi4WQeMW1KCK3aljaKLMZ57yb5Bsu+P3odyBk1AvYIPvdajAJiiikRdIDmi58dqfN0vAkEAjFX8LwjbCg+aaB5gvsA3t6ynxhBJcWb4UZQtD0zdRzhKLMuaBn05rKssjnuSaRuSgPaHe5OkOjx6yIiOuz98iQJAXIDpSMYhm5lsFiITPDScWzOLLnUR55HL/biaB1zqoODj2so7G2JoTiYiznamF9h9GuFC2TablbINq80U2NcxxQJBAMhw06Ha/U7qTjtAmr2qAuWSWvHU4ANu2h0RxYlKTpmWgO0f47jCOQhdC3T/RK7f38c7q8uPyi35eZ7S1e/PznY=";
+
+    //TODO !!!! 注：该公钥为测试账号公钥  开发者必须设置自己的公钥 ,否则会存在安全隐患
+    public static final String PUBLIC_KEY        = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCitD16CypwZILTpdJL8nPV9rVFHYf5UWa/URNX6469mbQLpWfjKM/VSWRXsNVGSM3itOO/KG2Pw4x5g9xjH6iaE4LlaidjBIPpifISSlnpbyi4HxQTZYgMPv/TuiWofUN5kcwg/KQAQxB2OwTOeFu2i3LhqSCDmv6koTvHW15/hQIDAQAB";
+
+    /**支付宝网关*/
+    public static final String ALIPAY_GATEWAY    = "https://openapi.alipay.com/gateway.do";
+
+    /**授权访问令牌的授权类型*/
+    public static final String GRANT_TYPE        = "authorization_code";
+}
